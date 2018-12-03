574a6721842ac03cb983385fdd248a336143c6d3
Hendi Santika <hendisantika@gmail.com>
2018-12-04 03:58:44 +0700

    #4 - Add modul qrcode-gen-web

---
+++ qrcode-gen-web/src/main/java/com/hendisantika/qrcode/qrcodegenweb/QrcodeGenWebApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.qrcode.qrcodegenweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeGenWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeGenWebApplication.class, args);
+    }
+}
---
+++ qrcode-gen-web/src/test/java/com/hendisantika/qrcode/qrcodegenweb/QrcodeGenWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.qrcode.qrcodegenweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QrcodeGenWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
